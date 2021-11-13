3cb92e069dee5c17a55f75cbdff6cfea41663221
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-13 15:42:31 -0800

    added solution

--- challenges/factorial/js/factorial.js
+++ challenges/factorial/js/factorial.js
@@ -1,2 +1,7 @@
-exports.factorial = function(num) {
+exports.factorial = function (num) {
+    if (num === 0) {
+        return 1;
+    } else {
+        return num * exports.factorial(num - 1);
+    }
 };

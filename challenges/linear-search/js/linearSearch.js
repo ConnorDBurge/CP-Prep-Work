5f6feac4a080c7304f5b63e4481f415835619ed2
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-13 17:06:36 -0800

    solution for release 0: Linear Serach

--- challenges/linear-search/js/linearSearch.js
+++ challenges/linear-search/js/linearSearch.js
@@ -1,9 +1,16 @@
-const arrayToSearchThrough = [];
-for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
-}
+// const arrayToSearchThrough = []; // empty list
+// for (let i = 1; i <= 1000; i++) { // pushes 1 . . 1,000 into list
+//     arrayToSearchThrough.push(i);
+// }
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+exports.linearSearch = function (valueToFind, arrayToSearchThrough) {
+    let index = 0;
+    for (let i = 0; i <= arrayToSearchThrough.length; i++) {
+        if (valueToFind === arrayToSearchThrough[i]) {
+            return index;
+        }
+        index++;
+    }
+    return undefined;
 };
 

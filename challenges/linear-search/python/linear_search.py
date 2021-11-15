7ebdd621d7f813618d174c51c5117d6c1bbd5721
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-14 18:31:18 -0800

    added linear_search.py solution

--- challenges/linear-search/python/linear_search.py
+++ challenges/linear-search/python/linear_search.py
@@ -2,5 +2,20 @@ array_to_search_through = []
 for number in range(1, 1001):
     array_to_search_through.append(number)
 
+
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    index = 0
+    for value in array_to_search_through:
+        if value == value_to_find:
+            return index
+        index += 1
+
+
+def global_linear_search(value_to_find, array_to_search_through):
+    index = 0
+    indices = []
+    for value in array_to_search_through:
+        if value == value_to_find:
+            indices.append(index)
+        index += 1
+    return indices

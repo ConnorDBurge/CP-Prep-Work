5722b04b6918e9a328d5446ccbc238565e60e524
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-14 17:38:31 -0800

    99_Bottles Python solution added

--- challenges/99-bottles/99_bottles.py
+++ challenges/99-bottles/99_bottles.py
@@ -1,5 +1,13 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+    for i in range(num, 0, -1):
+        print(
+            f'{i} bottle{"s" if i > 1 else ""} of beer on the wall, {i} bottle{"s" if i > 1 else ""} of beer.')
+        if i > 1:
+            print(
+                f'Take one down and pass it around, {i - 1} bottle{"s" if i - 1 > 1 else ""} of beer on the wall.')
+
+    print(
+        f'Take one down and pass it around, no more bottles of beer on the wall. \nNo more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, {num} bottles of beer on the wall.')
 
 
-bottle_song()
+bottle_song(5)

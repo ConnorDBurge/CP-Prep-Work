730944aa463237b07b44e0a9c9dabed39a4f7d94
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-20 21:50:01 -0800

    finished Anagrams in Py

--- challenges/anagrams/python/character_match.py
+++ challenges/anagrams/python/character_match.py
@@ -1,5 +1,26 @@
-# Don't forget to run your tests!
+# Strips whitespace from strings
+def stringStripper(string):
+    stringList = []
+    for char in string:
+        if char != ' ':
+            stringList.append(char)
+    return stringList
+
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+
+    # get raw characters with no whitespace
+    string1Split = stringStripper(string1.lower())
+    string2Split = stringStripper(string2.lower())
+
+    # first, check len of strings
+    if len(string1Split) == len(string2Split):
+        # checks characters in strings
+        for char in string1Split:
+            if char not in string2Split:
+                return False
+            else:
+                return True
+
+    # returns if strings are not equal size
+    return False
--- challenges/anagrams/python/character_match_spec.py
+++ challenges/anagrams/python/character_match_spec.py
@@ -1,13 +1,50 @@
 from character_match import is_character_match
-
-# This should return a bunch of trues
-print(is_character_match('charm', 'march') == True)
-print(is_character_match('zach', 'attack') == False)
-print(is_character_match('CharM', 'mARcH') == True)
-print(is_character_match('abcde2', 'c2abed') == True)
-
-print("This test is for the challenge quesiton")
-print(is_character_match('Anna Madrigal', 'A man and a girl') == True)
+import unittest
 
 
-# Can you translate this driver code to unit tests?
+class CharacterMatchUnitTest(unittest.TestCase):
+    'Unit test for character_match.py'
+
+    def test_return_type_bool(self):
+        'returns type bool'
+        test = is_character_match('charm', 'march')
+        self.assertIsInstance(test, bool)
+
+    def test_for_same_len(self):
+        'test for same length strings'
+        test = is_character_match('charm', 'march')
+        self.assertEqual(test, True)
+
+    def test_for_same_len(self):
+        'test an empty string'
+        test = is_character_match('', '')
+        self.assertEqual(test, False)
+
+    def test_return_true_for_true_anagram_1(self):
+        'returns true for valid anagram'
+        test = is_character_match('charm', 'march')
+        self.assertEqual(test, True)
+
+    def test_return_true_for_true_anagram_2(self):
+        'returns true for valid anagram'
+        test = is_character_match('CharM', 'mARcH')
+        self.assertEqual(test, True)
+
+    def test_return_true_for_true_anagram_3(self):
+        'returns true for valid anagram'
+        test = is_character_match('abcde2', 'c2abed')
+        self.assertEqual(test, True)
+
+    def test_return_true_for_true_anagram_4(self):
+        'returns true for non-valid anagram'
+        test = is_character_match('zach', 'attack')
+        self.assertEqual(test, False)
+
+    def test_return_true_for_True_anagram_5(self):
+        'returns true for valid anagram'
+        test = is_character_match('Anna Madrigal', 'A man and a girl')
+        self.assertEqual(test, True)
+
+
+if __name__ == '__main__':
+    unittest.main()

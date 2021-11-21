d8a83334b48c7fba1756b2a8a8100d452a51cd49
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-20 17:08:47 -0800

    final solution armstrongNumbers.js

--- lectures/TestDrivenDevelopment/hello.test.js
+++ lectures/TestDrivenDevelopment/hello.test.js
@@ -1,4 +1,5 @@
 let { sum, multiply } = require("./hello.js");
+let { describe, expect, test } = require('@jest/globals');
 
 test("test that 1 + 3 + 5 == 9", () => {
     expect(sum(1, 3, 5)).toBe(9)
@@ -7,4 +8,3 @@ test("test that 1 + 3 + 5 == 9", () => {
 test("test that 1 * 3 * 5 == 15", () => {
     expect(multiply(1, 3, 5)).toBe(15)
 })
-

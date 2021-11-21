562bf5e14ba9755ede3b6ec8a903d2976e6fca0e
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-21 03:08:03 -0800

    finished anagrams in JS

--- challenges/anagrams/js/characterMatch.js
+++ challenges/anagrams/js/characterMatch.js
@@ -1,3 +1,34 @@
-exports.isCharacterMatch = function(string1, string2) {
+exports.isCharacterMatch = function (string1, string2) {
 
-};
+    // get raw characters with no whitespace
+    string1list = stringStripper(string1.toLowerCase())
+    string2list = stringStripper(string2.toLowerCase())
+
+    // first, check len of strings
+    if (string1list.length === string2list.length) {
+        // checks characters in strings
+        for (let i = 0; i < string1list.length; i++) {
+            for (let j = 0; j < string2list.length; j++) {
+                if (string2list.includes(string1list[i])) {
+                    continue
+                } else {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    return false
+}
+
+
+const stringStripper = (string) => {
+    result = string.split('')
+    stripped = []
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] !== ' ') {
+            stripped.push(result[i])
+        }
+    }
+    return stripped
+}
--- challenges/anagrams/js/characterMatch.test.js
+++ challenges/anagrams/js/characterMatch.test.js
@@ -1,11 +1,54 @@
 // Can you translate this driver code to unit tests?
+var ana = require("./characterMatch");
+const { describe, test, expect } = require('@jest/globals');
 
-var ana = require("./character_match");
+test('return type is boolean', () => {
+    result = ana.isCharacterMatch('charm', 'march')
+    expect(typeof result).toBe('boolean')
+})
 
-console.log(ana.isCharacterMatch('charm', 'march') === true);
-console.log(ana.isCharacterMatch('zach', 'attack') === false);
-console.log(ana.isCharacterMatch('CharM', 'mARcH') === true);
-console.log(ana.isCharacterMatch('abcde2', 'c2abed') === true);
+test('test for `` and ``', () => {
+    result = ana.isCharacterMatch('', '')
+    expect(result).toBeTruthy()
+})
 
-console.log("This test is for the challenge quesiton");
-console.log(ana.isCharacterMatch('Anna Madrigal', 'A man and a girl') === true);
+test('test for `` and `march`', () => {
+    result = ana.isCharacterMatch('', 'march')
+    expect(result).toBeFalsy()
+})
+
+test('test for `charm` and `march`', () => {
+    result = ana.isCharacterMatch('charm', 'march')
+    expect(result).toBeTruthy()
+})
+
+test('test for `CharM` and `mARcH`', () => {
+    result = ana.isCharacterMatch('CharM', 'mARcH')
+    expect(result).toBeTruthy()
+})
+
+test('test for `abcde2` and `c2abed`', () => {
+    result = ana.isCharacterMatch('abcde2', 'c2abed')
+    expect(result).toBeTruthy()
+})
+
+test('test for `zach` and `attack`', () => {
+    result = ana.isCharacterMatch('zach', 'attack')
+    expect(result).toBeFalsy()
+})
+
+test('test for `Anna Madrigal` and `A man and a girl`', () => {
+    result = ana.isCharacterMatch('Anna Madrigal', 'A man and a girl')
+    expect(result).toBeTruthy()
+})
+
+
+
+
+// console.log(ana.isCharacterMatch('charm', 'march') === true);
+// console.log(ana.isCharacterMatch('zach', 'attack') === false);
+// console.log(ana.isCharacterMatch('CharM', 'mARcH') === true);
+// console.log(ana.isCharacterMatch('abcde2', 'c2abed') === true);
+
+// console.log("This test is for the challenge quesiton");
+// console.log(ana.isCharacterMatch('Anna Madrigal', 'A man and a girl') === true);

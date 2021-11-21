c9baf59fcb498cc20e549063ff7a7690366fb827
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-20 19:53:09 -0800

    Final solution for Sum Pairs in JS

--- challenges/sum-pairs/js/sumPairs.js
+++ challenges/sum-pairs/js/sumPairs.js
@@ -1,4 +1,38 @@
-exports.sumPairs = function () {
-    sum_pairs_list = [] // holds sum pairs
+exports.sumPairs = function (array, number) {
+    const sum_pairs_list = [] // holds sum pairs
+
+    for (let i = 0; i < array.length; i++) { // fixed number
+        for (let j = 0; j < array.length; j++) { // check number
+            if (array[i] + array[j] == number) {
+                if (checkArray(sum_pairs_list, [array[j], array[i]])) {
+                    return sum_pairs_list
+                } else {
+                    sum_pairs_list.push([array[i], array[j]])
+                }
+            }
+        }
+    }
+    if (sum_pairs_list.length === 0) {
+        return 'unable to find pairs'
+    }
     return sum_pairs_list
-};
+}
+
+const checkArray = (array, testArray) => {
+    for (let i = 0; i < array.length; i++) { // iterates over each array
+        let checker = false // records if arrays at index are the same
+        for (let j = 0; j < array[i].length; j++) {  // iterates over array elements
+            // if combo of [n1, n2] are found then set checker to true
+            if (array[i][j] === testArray[j]) {
+                checker = true
+                // as soon a new value that is not in array is found, checker is false
+            } else {
+                checker = false
+            }
+        }
+        if (checker) {
+            return true
+        }
+    }
+    return false
+}
--- challenges/sum-pairs/js/sumPairs.test.js
+++ challenges/sum-pairs/js/sumPairs.test.js
@@ -1,9 +1,34 @@
-var sp = require("./sumPairs");
-var { detect, expect, test } = require('@jest/globals')
+const sp = require("./sumPairs");
+const { detect, expect, test } = require('@jest/globals')
 
-// Don't forget to add your tests :)
+// run test cases using 'npm run test'
+
 test('returns a list object', () => {
-    received = sp.sumPairs()
+    received = sp.sumPairs([0], 0)
     expected = Array
     expect(received).toBeInstanceOf(expected)
+})
+
+test('test an empty list', () => {
+    received = sp.sumPairs([], 20)
+    expected = 'unable to find pairs'
+    expect(received).toStrictEqual(expected)
+})
+
+test('returns sum pairs for 9', () => {
+    received = sp.sumPairs([1, 2, 3, 4, 5], 9)
+    expected = [[4, 5]]
+    expect(received).toStrictEqual(expected)
+})
+
+test('returns sum pairs for 7', () => {
+    received = sp.sumPairs([1, 2, 3, 4, 5], 7)
+    expected = [[2, 5], [3, 4]]
+    expect(received).toStrictEqual(expected)
+})
+
+test('returns no sum paris 27', () => {
+    received = sp.sumPairs([3, 1, 5, 8, 2], 27)
+    expected = 'unable to find pairs'
+    expect(received).toStrictEqual(expected)
 })

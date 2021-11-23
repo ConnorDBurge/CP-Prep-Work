5c97f7a92a1656082068d67c7cad722af66ede80
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-22 20:28:41 -0800

    final anagram2 solution

--- challenges/anagrams2/python/anagram2.py
+++ challenges/anagrams2/python/anagram2.py
@@ -1,4 +1,24 @@
-# Don't forget to run the tests (and create some of your own)
+def is_character_match(string1, string2):  # Performs anagram test
+
+    # get raw character list with no whitespace to compare against each other
+    string1Split = [char for char in string1.lower() if char != ' ']
+    string2Split = [char for char in string2.lower() if char != ' ']
+
+    # first, check len of strings, if false then they are not anagrams
+    if len(string1Split) == len(string2Split):
+        # checks characters in strings
+        for char in string1Split:
+            if char not in string2Split:
+                return False  # false when different char is found
+            else:
+                return True
+
+    # returns if strings are not equal size
+    return False
+
 
 def anagrams_for(word, list_of_words):
-    # your code here
+    # uses logic from anagrams1 challenge to perform anagram test
+    anagrams = list(
+        filter(lambda ana: is_character_match(word, ana), list_of_words))
+    return anagrams
--- challenges/anagrams2/python/anagram2_spec.py
+++ challenges/anagrams2/python/anagram2_spec.py
@@ -1,8 +1,36 @@
-# Can you translate this driver code to unit tests?
+from anagram2 import anagrams_for, is_character_match
+import unittest
 
-from anagram2 import anagrams_for 
 
-list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
+class Anagrams(unittest.TestCase):
+    'Unittest for anagrams.py'
+    list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
 
-print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
-print(anagrams_for("apple", list_of_words) == [])
+    def test_for_returned_list(self):
+        received = anagrams_for('threads', self.list_of_words)
+        self.assertIsInstance(received, list)
+
+    def test_for_is_character_match(self):
+        received = is_character_match('trashed', 'hardest')
+        expected = True
+        self.assertEqual(received, expected)
+
+    def test_returns_a_anagram_list(self):
+        received = anagrams_for('threads', self.list_of_words)
+        expected = ["threads", "trashed", "hardest", "hatreds"]
+        self.assertEqual(received, expected)
+
+    def test_returns_an_empty_list(self):
+        received = anagrams_for("apple", self.list_of_words)
+        expected = []
+        self.assertEqual(received, expected)
+
+    def test_return_true_for_whitespace(self):
+        list_words = ['A man and a girl', 'Aman and a girl', 'Aand a gi']
+        received = anagrams_for('Anna Madrigal', list_words)
+        expected = ['A man and a girl', 'Aman and a girl']
+        self.assertEqual(received, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()

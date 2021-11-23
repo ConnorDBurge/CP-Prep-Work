2a888d8fae7ee70e80cf54c43e2ea5f62323ff14
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-22 21:54:54 -0800

    final solution for anagrams2 in JS

--- challenges/anagrams2/js/anagram2.js
+++ challenges/anagrams2/js/anagram2.js
@@ -1,3 +1,32 @@
-exports.anagramsFor = function(word, listOfWords) {
+const characterMatch = (string1, string2) => {
+    // get raw characters with no whitespace
+    const string1list = string1.split('').filter((char) => {
+        return char !== ' '
+    })
+    const string2list = string2.split('').filter((char) => {
+        return char !== ' '
+    })
 
-};
+    // first, check len of strings
+    if (string1list.length === string2list.length) {
+        // checks characters in strings
+        for (let i = 0; i < string1list.length; i++) {
+            for (let j = 0; j < string2list.length; j++) {
+                if (!string2list.includes(string1list[i])) {
+                    return false // false when different char is found
+                }
+            }
+        }
+        return true
+    }
+    return false // returns if strings are not equal size
+}
+
+
+const anagramsFor = (word, listOfWords) => {
+    // uses logic from anagrams1 challenge to perform anagram test
+    anagrams = listOfWords.filter(ana => characterMatch(word, ana))
+    return anagrams
+}
+
+module.exports = { anagramsFor }

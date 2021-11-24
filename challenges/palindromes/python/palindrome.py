73d686b29bd93ab45f1d3809db459f174c2bf92e
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-23 22:06:15 -0800

    final solution palindrome.py

---
+++ challenges/palindromes/python/palindrome.py
@@ -0,0 +1,12 @@
+import re
+
+
+def palindrome(word):
+    chars = [char for char in str(word).lower() if re.match('[a-z1-9]', char)]
+    while len(chars) > 1:
+        if chars[0] != chars[-1]:
+            return False
+        else:
+            chars.pop(0)
+            chars.pop(-1)
+    return True
---
+++ challenges/palindromes/python/palindrome_spec.py
@@ -0,0 +1,50 @@
+from palindrome import palindrome
+import unittest
+
+
+class PalindromeTestCase(unittest.TestCase):
+    'Test case for palindrome.py'
+
+    def test_return_type(self):
+        received = palindrome('racecar')
+        self.assertIsInstance(received, bool)
+
+    def test_return_true_1(self):
+        received = palindrome('racecar')
+        self.assertEqual(received, True)
+
+    def test_return_true_2(self):
+        received = palindrome('Noon')
+        self.assertEqual(received, True)
+
+    def test_return_true_3(self):
+        received = palindrome('ciVic')
+        self.assertEqual(received, True)
+
+    def test_return_true_4(self):
+        received = palindrome(434)
+        self.assertEqual(received, True)
+
+    def test_return_true_5(self):
+        received = palindrome('A man, a plan, a canal -- Panama')
+        self.assertEqual(received, True)
+
+    def test_return_true_6(self):
+        received = palindrome('Sore was I ere I saw Eros.')
+        self.assertEqual(received, True)
+
+    def test_return_false_1(self):
+        received = palindrome('nice')
+        self.assertEqual(received, False)
+
+    def test_return_false_2(self):
+        received = palindrome(123)
+        self.assertEqual(received, False)
+
+    def test_return_false_3(self):
+        received = palindrome('bomb')
+        self.assertEqual(received, False)
+
+
+if __name__ == '__main__':
+    unittest.main()

7a03c1771d86f220b7e19f907844ef9b147d2c6b
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-23 20:01:27 -0800

    char_count in JavaScript

--- challenges/char-count/js/charCount.js
+++ challenges/char-count/js/charCount.js
@@ -1,3 +1,15 @@
-exports.charCount = function() {
-
+const charCount = (str) => {
+    const counts = {}
+    const letters = str.split('').filter((char) => {
+        return char !== ' '
+    })
+    for (let char of letters) {
+        counts[char] += 1
+        if (isNaN(counts[char])) {
+            counts[char] = 1
+        }
+    }
+    return counts
 };
+
+module.exports = { charCount }

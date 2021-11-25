35e6965ddf75326c07ed478157f113943c7a74f9
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-24 20:17:15 -0800

    solution for pad_array.py

--- challenges/pad-array/python/pad_array.py
+++ challenges/pad-array/python/pad_array.py
@@ -1,2 +1,13 @@
-#REMEMBER TO PSEUDOCODE
-def pad(array, min_size, value = None):
+def pad(array, min_size, value=None):
+    padded = array
+
+    if min_size <= len(array):
+        return padded
+
+    for i in range(0, min_size):
+        try:
+            array[i] = array[i]
+        except IndexError:
+            array.append(value)
+
+    return padded
--- challenges/pad-array/python/pad_array_spec.py
+++ challenges/pad-array/python/pad_array_spec.py
@@ -1,1 +1,30 @@
-# Write unit tests!
+from pad_array import pad
+import unittest
+
+
+class PadTestCase(unittest.TestCase):
+    'Unittest for pad.py'
+
+    def test_return_type(self):
+        received = pad([1, 2, 3], 5)
+        self.assertIsInstance(received, list)
+
+    def test_return_correct_list_1(self):
+        received = pad([1, 2, 3], 3)
+        self.assertEqual(received, [1, 2, 3])
+
+    def test_return_correct_list_2(self):
+        received = pad([1, 2, 3], 5)
+        self.assertEqual(received, [1, 2, 3, None, None])
+
+    def test_return_correct_list_3(self):
+        received = pad([1, 2, 3], 5, 'apple')
+        self.assertEqual(received, [1, 2, 3, 'apple', 'apple'])
+
+    def test_return_correct_list_4(self):
+        received = pad([1, 2, 3], 0, 'apple')
+        self.assertEqual(received, [1, 2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()

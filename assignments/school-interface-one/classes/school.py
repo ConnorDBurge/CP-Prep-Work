8c47e2df0d7be2d7add71f7a823457ed282f3704
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-27 19:12:30 -0800

    school-interface-one

--- assignments/school-interface-one/classes/school.py
+++ assignments/school-interface-one/classes/school.py
@@ -7,3 +7,12 @@ class School:
         self.name = name
         self.staff = Staff.all_staff()
         self.students = Student.all_students()
+
+    def list_students(self):
+        for student in self.students:  # for each Student object
+            print(f'{student.name} {student.school_id}')
+
+    def find_student_by_id(self, student_id):
+        for student in self.students:
+            if student.school_id == student_id:
+                return student

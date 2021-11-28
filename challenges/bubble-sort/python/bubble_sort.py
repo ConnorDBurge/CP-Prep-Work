1d5fc8584f92b858d9ef462c153cee99537f1620
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-28 10:27:06 -0800

    bubble-sort v1

--- challenges/bubble-sort/python/bubble_sort.py
+++ challenges/bubble-sort/python/bubble_sort.py
@@ -1,7 +1,26 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
+class BubbleSort:
 
-# Your Code Here
+    def __init__(self, array):
+        self.sorted = self.sort(array)
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+    def sort(self, array):
+        self.swaps = 0  # track number of swaps
+        sorted = array
+        prev = 0
+        current = 1
+        while current < len(sorted):
+            if sorted[current] < sorted[prev]:
+                moved = sorted[current]
+                sorted[current] = sorted[prev]
+                sorted[prev] = moved
+                prev = 0
+                current = 1
+                self.swaps += 1
+            else:
+                prev += 1
+                current += 1
+        return sorted
+
+
+# print(f"Final result: {result}")
+# print(f"Swaps: {swaps}")

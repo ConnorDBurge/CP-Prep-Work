78fae2c4426b46ded304da54efe63f69962b4edc
ConnorDBurge <connor.d.burge@gmail.com>
2021-11-27 19:22:40 -0800

    school-interface-one

--- assignments/school-interface-one/classes/student.py
+++ assignments/school-interface-one/classes/student.py
@@ -7,6 +7,9 @@ class Student(Person):
         super().__init__(name, age, role, password)
         self.school_id = school_id
 
+    def __str__(self):
+        return f'\n{self.name.upper()}\n---------------\nage: {self.age}\nid: {self.school_id}'
+
     @classmethod
     def all_students(cls):
         # return list of Student objects

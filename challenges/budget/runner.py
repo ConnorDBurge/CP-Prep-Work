a13fda08ddfa6ccde3a685e7022596bfc9f0ffcd
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-05 10:44:29 -0800

    added new_transaction()

--- challenges/budget/runner.py
+++ challenges/budget/runner.py
@@ -1,5 +1,9 @@
 from classes.budget import Budget
 
 budget = Budget('December')
+budget.new_transaction('bills', 'Netflix', '4')
+
+for transaction in budget.transactions:
+    print(transaction.__dict__)
 
 print(str(budget))

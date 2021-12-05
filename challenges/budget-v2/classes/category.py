5a2856ac41ccfc0b1f8a894d8c34fdf19fb31557
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-05 13:45:14 -0800

    created Category class

--- challenges/budget-v2/classes/category.py
+++ challenges/budget-v2/classes/category.py
@@ -3,4 +3,35 @@ from classes.transaction import Transaction
 
 class Category:
 
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.total = 0
+        self.transactions = list()
+
+    def transaction(self, name, amount):
+        new_transaction = Transaction(self.name, name, amount)
+        for transaction in self.transactions:
+            if transaction.name == name:
+                self.transactions.remove(transaction)
+                self.transactions.append(new_transaction)
+                break
+        else:
+            self.transactions.append(new_transaction)
+
+        self.sum()
+        return new_transaction
+
+    def sum(self):
+        sum = 0
+        for transaction in self.transactions:
+            sum += transaction.amount
+        self.total = sum
+        return self.total
+
+    def str(self, percent):
+        string = f'--------------------------------\n{self.name.upper() + f" - {percent:.0%}":<22}' + \
+            f'$ {self.total:,.2f}'.rjust(10)
+        for transaction in self.transactions:
+            string += f'\n{transaction.name:<22}' + \
+                f'$ {transaction.amount:,.2f}'.rjust(10)
+        return string
--- challenges/budget-v2/runner.py
+++ challenges/budget-v2/runner.py
@@ -1,7 +1,15 @@
-from classes.transaction import Transaction
+from classes.category import Category
 
-transaction1 = Transaction('income', 'Salary 2', '1500')
-transaction2 = Transaction('bills', 'Cox', '178')
+income = Category('income')
+income.transaction('Salary 1', '1317')
+income.transaction('Salary 2', '1500')
 
-print(str(transaction1))
-print(str(transaction2))
+bills = Category('bills')
+bills.transaction('Cox', '178')
+bills.transaction('Netflix', '4.00')
+bills.transaction('Netflix', '4.99')
+bills.transaction('USAA', '145.76')
+
+
+print(income.str(0))
+print(bills.str(0))

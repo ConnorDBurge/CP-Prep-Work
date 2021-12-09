dad5e5ada02d264a2299996a0e08e063f2bbb900
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-09 09:47:51 -0800

    Recurion solutions in Python

--- challenges/recursion/python/recursion_challenge.py
+++ challenges/recursion/python/recursion_challenge.py
@@ -1,13 +1,43 @@
+import re
+
+
 class Recursion:
+    count = 0
 
-    def factorial(x):
+    def factorial(self, x):
+        if x == 0:
+            return 1
+        return x * self.factorial(x - 1)
+
+    def palindrome(self, string):
+        chars = [char for char in str(
+            string).lower() if re.match('[a-z0-9]', char)]
+
+        if len(chars) <= 1:
+            return True
+
+        if chars[0] != chars[-1]:
+            return False
+
+        return self.palindrome(chars[1:-1])
+
+    def bottles(self, num):
+        self.count += 1
+        print(
+            f'{num} bottle{"s" if num > 1 else ""} of beer on the wall, {num} bottle{"s" if num > 1 else ""} of beer.')
+        if num > 1:
+            print(
+                f'Take one down and pass it around, {num - 1} bottle{"s" if num - 1 > 1 else ""} of beer on the wall.')
+
+        if num < 1:
+            return print(
+                f'Take one down and pass it around, no more bottles of beer on the wall. \nNo more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, {self.count - 1} bottles of beer on the wall.')
+
+        self.bottles(num - 1)
+
+    def roman_num(self, num):
         pass
 
-    def palindrome(string):
-        pass
 
-    def bottles(num):
-        pass
-
-    def roman_num(num):
-        pass
+recursion = Recursion()
+recursion.bottles(10)
--- challenges/recursion/python/recursion_spec.py
+++ challenges/recursion/python/recursion_spec.py
@@ -3,7 +3,14 @@ import unittest
 
 
 class RecursionTest(unittest.TestCase):
-    pass
+    recursion = Recursion()
+
+    def test_factorial(self):
+        self.assertEqual(self.recursion.factorial(10), 3628800)
+
+    def test_palindrome(self):
+        self.recursion.palindrome('racecar')
+        self.assertEqual(self.recursion.palindrome('racecar'), True)
 
 
 if __name__ == '__main__':

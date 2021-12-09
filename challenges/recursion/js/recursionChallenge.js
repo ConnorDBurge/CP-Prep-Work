b99fcfd953fc7762da74bc84474bc74c4fd21592
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-09 10:03:37 -0800

    Recursion challenge in Javascript

--- challenges/recursion/js/recursionChallenge.js
+++ challenges/recursion/js/recursionChallenge.js
@@ -1,15 +1,31 @@
-exports.factorial = function() {
+exports.factorial = (num) => {
+    if (num === 0) {
+        return 1
+    } else {
+        return num * this.factorial(num - 1)
+    }
+};
+
+exports.palindrome = (string) => {
+    let word = string.toLowerCase()
+    const chars = word.split('').filter((char) => {
+        return char.match('[a-z0-9]')
+    })
+    if (chars.length <= 1) {
+        return true
+    }
+    if (chars[0] !== chars[chars.length - 1]) {
+        return false
+    }
+    return this.palindrome(string.slice(1, string.length - 1))
+
 
 };
 
-exports.palindrome = function() {
+exports.bottles = (num) => {
 
 };
 
-exports.bottles = function() {
-
-};
-
-exports.romanNum = function() {
+exports.romanNum = function () {
 
 };

3955b7094cb41d892e8439eaefb7ec2a2548d615
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-19 13:39:42 -0800

    binary search using recursion

--- challenges/binary-search/python/binary_search.py
+++ challenges/binary-search/python/binary_search.py
@@ -1,15 +1,15 @@
 def binary_search(target, arr):
-    left = 0
-    right = len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2  # make sure to round it down
-        if arr[mid] == target:
-            return mid
-        elif target < arr[mid]:
-            right = mid - 1
+    try:
+        mid = len(arr) // 2  # mid index
+        mid_value = arr[mid]  # mid value
+        if target > mid_value:
+            return binary_search(target, arr[mid:])
+        elif target < mid_value:
+            return binary_search(target, arr[:mid])
         else:
-            left = mid + 1
-    return -1
+            return True
+    except RecursionError:
+        return False
 
 
-print(binary_search(4, [-2, 3, 4, 7, 8, 9, 11, 13]))
+print(binary_search(14, [-2, 3, 4, 7, 8, 9, 11, 13]))

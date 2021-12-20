434b36d1fd53f7a0009fd0ef40385edca8db9993
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-20 10:12:40 -0800

    started ice-cream challenge

---
+++ challenges/ice-cream-and-freezers/ice_cream_and_freezers.py
@@ -0,0 +1,10 @@
+class IceCream:
+    pass
+
+
+class Freezer:
+    pass
+
+
+# Will you need other classes? What attributes will those classes have?
+# How will classes talk to each other?
---
+++ challenges/ice-cream-and-freezers/ice_cream_and_freezers_spec.py
@@ -0,0 +1,11 @@
+import unittest
+from ice_cream_and_freezers import IceCream, Freezer
+
+
+class TestIceCreamAndFreezers(unittest.TestCase):
+    # Go ahead and write some tests here for your code. We know that you'll at least need the IceCream and Freezer class.
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()

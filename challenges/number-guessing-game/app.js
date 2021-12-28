29d4395bcd1566663947d1d4c09ecd97866aeab7
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-28 13:00:39 -0800

    Guessing Game, Toms Solution

--- challenges/number-guessing-game/app.js
+++ challenges/number-guessing-game/app.js
@@ -1,2 +1,51 @@
-console.log("HELLO OSCAR PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+let random = 0;
+const pickRandomNumber = () => {
+    random = Math.ceil(Math.random() * 100);
+    console.log(random);
+}
+pickRandomNumber(); // runs on load page
+
+document.getElementById('submit').onclick = () => {
+    const inputNumber = document.getElementById('input-number')
+    let userNumber = inputNumber.value;
+    inputNumber.value = '';
+    addNumberToList(userNumber);
+    return checkNumber(+userNumber);
+}
+
+const checkNumber = (number) => {
+    let message = document.getElementById('game-message');
+    if (number === random) {
+        message.innerHTML = 'You won!'
+    } else if (number > random) {
+        message.innerHTML = 'Guess Lower!'
+    } else {
+        message.innerHTML = 'Guess Higher!'
+    }
+}
+
+const addNumberToList = (number) => {
+    let newElem = document.createElement('LI');
+    const node = document.createTextNode(number);
+    newElem.appendChild(node);
+    const list = document.getElementById('number-list');
+    list.appendChild(newElem);
+}
+
+const doesUserExist = () => {
+    if (localStorage.getItem('user-name')) {
+        let name = localStorage.getItem('user-name');
+        document.getElementById('welcome').innerHTML = `Welcome ${name}!`;
+        return document.getElementById('get-username').remove();
+    }
+}
+
+document.getElementById('submit-username').onclick = () => {
+    const userName = document.getElementById('user-name').value;
+    if (userName.length > 0) {
+        localStorage.setItem('user-name', document.getElementById('user-name').value);
+        document.getElementById('user-name').value = '';
+        return doesUserExist();
+    }
+}
+doesUserExist();

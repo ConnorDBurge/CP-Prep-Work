99dae0f878563c0b5a66d99a45b0dcdd5e2dbffe
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-29 15:11:08 -0800

    Finished LikedIn Profile Header card

--- lectures/Flexbox/LinkedInNavbar/scripts.js
+++ lectures/Flexbox/LinkedInNavbar/scripts.js
@@ -6,3 +6,17 @@ const smallDropNav = () => {
         dropNav.style.display = 'flex';
     }
 }
+
+const showMore = () => {
+    let button = document.getElementById('more-btn');
+    let section = document.getElementById('profile-more');
+    if (button.value === 'More') {
+        section.style.display = 'block';
+        button.style.backgroundColor = "#EBEBEB";
+        button.value = 'Close';
+    } else if (button.value === 'Close') {
+        section.style.display = 'none';
+        button.style.backgroundColor = "white";
+        button.value = 'More';
+    }
+}

f735a3add4205231e5919fe48655ec31d34f91bd
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-31 13:10:58 -0800

    HTTP Server, sends "Hello World" back on GET requests

--- challenges/http-server-one/server.py
+++ challenges/http-server-one/server.py
@@ -1,12 +1,18 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # so you don't have to change ports when restarting
+# so you don't have to change ports when restarting
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(('localhost', 9292))
 print('Waiting For Connection...')
 server.listen()
 
-client_connection, _client_address = server.accept()
-print('New Connection received!')
+while True:
+    client_connection, _client_address = server.accept()
+    data = client_connection.recv(1024).decode()
 
-client_connection.close()
+    print('GET request received')
+    message = "HTTP/1.0 200 OK\nContent-Type: text\plain\n\nHello World\n".encode()
+    client_connection.send(message)
+
+    client_connection.close()

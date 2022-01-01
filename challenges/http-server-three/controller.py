2610c192af58a452be482a7e800d602d4d1c7335
ConnorDBurge <connor.d.burge@gmail.com>
2021-12-31 18:06:59 -0800

    HTTP Server Three v1.0

--- challenges/http-server-three/controller.py
+++ challenges/http-server-three/controller.py
@@ -2,6 +2,8 @@ from classes.router import Router
 from classes.response import Response
 from classes.facts import Fact
 import datetime
+import re
+import csv
 
 
 @Router.route('/')
@@ -19,4 +21,15 @@ def time(request):
 @Router.route('/facts')
 def facts(request):
     response = Response('all_facts', {'facts': Fact.all_facts()})
+    return response
+
+
+@Router.route(r'\/facts\/(\d+)')
+def fact(request):
+    fact_id = re.match(r'\/facts\/(\d+)', request['Resource']).group(1)
+    csv_file = csv.reader(open('./facts.csv', "r"))
+    for row in csv_file:
+        if row[0] == fact_id:
+            fact = Fact(row[0], row[1])
+    response = Response('fact', {'fact': fact})
     return response

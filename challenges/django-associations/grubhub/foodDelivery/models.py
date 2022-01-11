7f8998e0279575e56ab7db7c5437bd2dfc4d60eb
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-10 19:27:27 -0800

    Grubhub Solution

--- challenges/django-associations/grubhub/foodDelivery/models.py
+++ challenges/django-associations/grubhub/foodDelivery/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+# And finally if you have set up your associations correctly a user should have many food items through orders. See the final test.
+
+class User(models.Model):
+    pass
+
+
+class Restaurant(models.Model):
+    pass
+
+
+class FoodItem(models.Model):
+    orders = models.ManyToManyField(
+        'Order', related_name='food_items', through='OrderFoodItem')
+
+
+class Order(models.Model):
+    # A user has many orders -- user.orders
+    # An order belongs to a user -- order.user
+    user = models.ForeignKey(
+        User, related_name="orders", on_delete=models.CASCADE)
+    # A restaurant has many orders
+    # An order belongs to a restaurant
+    restaurant = models.ForeignKey(
+        Restaurant, related_name="orders", on_delete=models.CASCADE)
+
+
+class OrderFoodItem(models.Model):
+    # An order has many order_food_items
+    # An order_food_item belongs to an order
+    order = models.ForeignKey(
+        Order, related_name="order_food_items", on_delete=models.CASCADE)
+    # A food item has many order_food_items
+    # An order_food_item belongs to a food_item
+    food_item = models.ForeignKey(
+        FoodItem, related_name="order_food_items", on_delete=models.CASCADE)
--- challenges/django-associations/grubhub/foodDelivery/tests.py
+++ challenges/django-associations/grubhub/foodDelivery/tests.py
@@ -2,23 +2,29 @@ from django.test import TestCase
 from .models import *
 import redgreenunittest as unittest
 
+
 class AssociationTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create()
         self.restaurant = Restaurant.objects.create()
-        self.order = Order.objects.create(user=self.user, restaurant=self.restaurant)
+        self.order = Order.objects.create(
+            user=self.user, restaurant=self.restaurant)
         self.food_item_1 = FoodItem.objects.create()
         self.food_item_2 = FoodItem.objects.create()
         self.food_item_3 = FoodItem.objects.create()
         self.food_item_4 = FoodItem.objects.create()
-        self.order_food_item_1 = OrderFoodItem.objects.create(order=self.order, food_item=self.food_item_1)
-        self.order_food_item_2 = OrderFoodItem.objects.create(order=self.order, food_item=self.food_item_2)
-        self.order_food_item_3 = OrderFoodItem.objects.create(order=self.order, food_item=self.food_item_3)
-        self.order_food_item_4 = OrderFoodItem.objects.create(order=self.order, food_item=self.food_item_4)
+        self.order_food_item_1 = OrderFoodItem.objects.create(
+            order=self.order, food_item=self.food_item_1)
+        self.order_food_item_2 = OrderFoodItem.objects.create(
+            order=self.order, food_item=self.food_item_2)
+        self.order_food_item_3 = OrderFoodItem.objects.create(
+            order=self.order, food_item=self.food_item_3)
+        self.order_food_item_4 = OrderFoodItem.objects.create(
+            order=self.order, food_item=self.food_item_4)
 
     def test_01_user_orders(self):
         """returns the user's orders"""
-        self.assertEqual(list(self.user.orders.all()),[self.order])
+        self.assertEqual(list(self.user.orders.all()), [self.order])
 
     def test_02_orders_user(self):
         """returns the order's user"""
@@ -34,7 +40,8 @@ class AssociationTestCase(TestCase):
 
     def test_05_orders_food_items(self):
         """returns the order's food items"""
-        self.assertEqual(list(self.order.food_items.all()), [self.food_item_1, self.food_item_2, self.food_item_3, self.food_item_4])
+        self.assertEqual(list(self.order.food_items.all()), [
+                         self.food_item_1, self.food_item_2, self.food_item_3, self.food_item_4])
 
     def test_06_food_item_orders(self):
         """returns all orders that contain the food item"""
@@ -45,5 +52,7 @@ class AssociationTestCase(TestCase):
 
     def test_07_users_food_items(self):
         """returns all the food items in the order"""
-        all_food_items = list(list((order.food_items.all()) for order in list(self.user.orders.all()))[0])
-        self.assertEqual(all_food_items, [self.food_item_1, self.food_item_2, self.food_item_3, self.food_item_4])
+        all_food_items = list(list((order.food_items.all())
+                              for order in list(self.user.orders.all()))[0])
+        self.assertEqual(all_food_items, [
+                         self.food_item_1, self.food_item_2, self.food_item_3, self.food_item_4])

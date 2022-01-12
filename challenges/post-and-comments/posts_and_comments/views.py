2b10bfd0a068c4c2e254d08e27f5e01822b78468
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-12 13:56:52 -0800

    Got the Django Backend peiced together

--- challenges/post-and-comments/posts_and_comments/views.py
+++ challenges/post-and-comments/posts_and_comments/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'p_and_c/index.html')
+
+
+def post_view(request, post_id):
+    return HttpResponse(f'This is the post view for the post with id: {post_id}')
+
+
+def comment(request, post_id):
+    return HttpResponse(f'This is the comment view for the post with id: {post_id}')

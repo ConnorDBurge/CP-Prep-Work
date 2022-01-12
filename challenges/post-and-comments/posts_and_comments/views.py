fface35daf8050578ae8a6f334d652e60cf25576
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-12 14:55:55 -0800

    index page is set-up and working

--- challenges/post-and-comments/posts_and_comments/views.py
+++ challenges/post-and-comments/posts_and_comments/views.py
@@ -1,13 +1,18 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    return render(request, 'p_and_c/index.html')
+    posts = Post.objects.all()
+    data = {'posts': posts}
+    return render(request, 'p_and_c/index.html', data)
 
 
 def post_view(request, post_id):
-    return HttpResponse(f'This is the post view for the post with id: {post_id}')
+    post = Post.objects.get(id=post_id)
+    data = {'post': post}
+    return render(request, 'p_and_c/post_view.html', data)
 
 
 def comment(request, post_id):

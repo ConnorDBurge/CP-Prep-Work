8f73d8c69361152b4698dd73cb23f4fec74f0b28
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-14 15:09:37 -0800

    done with ToDo functionality, absolutley no styling

--- challenges/django-todo/todo/urls.py
+++ challenges/django-todo/todo/urls.py
@@ -6,5 +6,7 @@ appname = 'todo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('new', views.create, name='create'),
-    path('<int:todo_id>', views.view, name='view')
+    path('<int:todo_id>', views.view, name='view'),
+    path('<int:todo_id>/edit', views.edit, name='edit'),
+    path('<int:todo_id>/delete', views.delete, name='delete')
 ]
--- challenges/django-todo/todo/views.py
+++ challenges/django-todo/todo/views.py
@@ -27,7 +27,30 @@ def create(request):
     return render(request, 'todo/new_todo.html', data)
 
 
+def edit(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    if request.method == "POST":
+        form = ToDoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save()
+            return redirect('index')
+    elif request.method == "GET":
+        form = ToDoForm(instance=todo)
+        data = {
+            'todo': todo,
+            'form': form,
+            'method': 'Edit ToDo'
+        }
+        return render(request, 'todo/edit_todo.html', data)
+
+
 def view(request, todo_id):
     todo = ToDo.objects.get(id=todo_id)
     data = {'todo': todo}
     return render(request, 'todo/view.html', data)
+
+
+def delete(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('index')

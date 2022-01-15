338e92e3d0e96a8f3b26a70a1d2270487bfa3055
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-15 11:44:32 -0800

    models made, and migrated

--- challenges/django-cars-and-brands/car_app/cars_and_brands/models.py
+++ challenges/django-cars-and-brands/car_app/cars_and_brands/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.brand_name
+
+
+class Car(models.Model):
+    car_model = models.CharField(max_length=200)
+    car_brand = models.ForeignKey(
+        Brand, related_name='cars', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.car_brand}: {self.car_model}'

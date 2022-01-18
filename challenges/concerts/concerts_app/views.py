27de63729cc1b472081fef31e31ab30c6c06f97e
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-17 20:55:11 -0800

    search results page is working, next styling

--- challenges/concerts/concerts_app/views.py
+++ challenges/concerts/concerts_app/views.py
@@ -10,10 +10,11 @@ def home(request):  # redirects to home page
 
 def search_results(request):
     search_topic = request.POST['query']
+    search_phrase = 'Everything' if search_topic == '' else search_topic
     response = requests.get(
         f"https://app.ticketmaster.com/discovery/v2/events.json?apikey=3QoGKoIxgONT7MoA5NrrA1XKKXaktmfM&keyword={search_topic}").json()
     events = list()
     events_json = response['_embedded']['events']
     for event in events_json:
         events.append(Event(event))
-    return render(request, 'concerts_pages/search_results.html', {'search_topic': search_topic, 'events': events})
+    return render(request, 'concerts_pages/search_results.html', {'search_phrase': search_phrase, 'events': events})

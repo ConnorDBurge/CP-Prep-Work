2dbfe22cd9adc0513194af49967fce03a46de87e
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-19 19:10:39 -0800

    finished detention

--- challenges/detention/src/App.js
+++ challenges/detention/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Detention from './components/Detention/Detention.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  writeDetention = () => {
+    let phrases = []
+    for (let i = 0; i < 100; i++) {
+      phrases.push(<Detention />)
+    }
+    return phrases
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        {this.writeDetention()}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ challenges/detention/src/components/Detention/Detention.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Detention extends React.Component {
+    render() {
+        return (
+            <div className="Detention">
+                <h1>I will never mutate state or props directly</h1>
+            </div>
+        )
+    }
+}
+
+export default Detention;

86886c60df29f0bff925cc8f6a4ed8179e7ac8da
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-22 11:11:58 -0800

    underscore word displaying

--- challenges/hangman/src/App.js
+++ challenges/hangman/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hang Man</h1>
-      <PuzzleWord />
+      <PuzzleWord puzzleWord={puzzleWord} />
       <GuessForm />
     </div>
   );

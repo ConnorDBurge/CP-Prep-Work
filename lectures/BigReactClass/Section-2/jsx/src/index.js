138b47177c5334af7c558630de8df162fe1f4ad4
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-22 13:52:04 -0800

    React Section 2 complete

---
+++ lectures/BigReactClass/Section-2/jsx/src/index.js
@@ -0,0 +1,23 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = () => {
+    const buttonText = 'Submit';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter Name:</label>
+            <input type="text" id="name" />
+            <button>{buttonText}</button>
+        </div>
+    )
+}
+
+
+// Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);

76099285afd158deed64adae4c8c78e7cd53b510
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-21 18:19:09 -0800

    mute button functional solution

--- challenges/mute-button/app/src/App.js
+++ challenges/mute-button/app/src/App.js
@@ -1,27 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import MuteButton from './components/MuteButton/MuteButton';
+import { useState } from 'react';
 
-class App extends Component {
-  state = {
-    isMuted: false
-  };
+// Class solution
+// class App extends Component {
+//   state = {
+//     isMuted: false
+//   };
 
-  toggleMute = () => {
-    this.setState({
-      isMuted: !this.state.isMuted
-    });
+//   toggleMute = () => {
+//     this.setState({
+//       isMuted: !this.state.isMuted
+//     });
+//   }
+
+//   render() {
+//     console.log(this.state)
+//     return (
+//       <div className="App">
+//         <MuteButton isMuted={this.state.isMuted} toggleMute={this.toggleMute} />
+//       </div>
+//     )
+//   }
+// }
+
+// Functional solution
+const App = () => {
+  const [isMuted, setIsMuted] = useState(false);
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
   }
 
-  render() {
-    console.log(this.state)
-    return (
-      <div className="App">
-        <MuteButton isMuted={this.state.isMuted} toggleMute={this.toggleMute} />
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <MuteButton isMuted={isMuted} toggleMute={toggleMute} />
+    </div>
+  )
+
 }
 
 export default App;
--- challenges/mute-button/app/src/components/MuteButton/MuteButton.js
+++ challenges/mute-button/app/src/components/MuteButton/MuteButton.js
@@ -2,17 +2,31 @@ import React from 'react';
 import OnIcon from '../../icons/on.svg';
 import OffIcon from '../../icons/off.svg';
 
-class MuteButton extends React.Component {
-    render() {
-        const { isMuted, toggleMute } = this.props;
-        return (
-            <button onClick={toggleMute}>
-                {isMuted ?
-                    <img src={OffIcon} alt='AudioOff' /> :
-                    <img src={OnIcon} alt='AudioOn' />}
-            </button>
-        );
-    }
+// // Class solution
+// class MuteButton extends React.Component {
+//     render() {
+//         const { isMuted, toggleMute } = this.props;
+//         return (
+//             <button onClick={toggleMute}>
+//                 {isMuted ?
+//                     <img src={OffIcon} alt='AudioOff' /> :
+//                     <img src={OnIcon} alt='AudioOn' />}
+//             </button>
+//         );
+//     }
+// }
+
+// Function solution
+const MuteButton = (props) => {
+    const { isMuted, toggleMute } = props;
+    return (
+        <button onClick={toggleMute}>
+            {isMuted ?
+                <img src={OffIcon} alt='AudioOff' /> :
+                <img src={OnIcon} alt='AudioOn' />}
+        </button>
+    );
 }
+
 
 export default MuteButton;

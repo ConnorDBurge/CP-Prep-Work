9683c1c33e4aae404cc57c34ab8197a0c30cac1a
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-26 19:12:55 -0800

    started section 11

---
+++ lectures/BigReactClass/Section-11/youtube/src/components/App/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import youtube from '../../API/youtube';
+
+
+class App extends Component {
+
+    state = {
+        videos: []
+    }
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        })
+        this.setState({ videos: response.data.items })
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                I have {this.state.videos.length} videos
+            </div>
+        );
+    }
+}
+
+export default App;

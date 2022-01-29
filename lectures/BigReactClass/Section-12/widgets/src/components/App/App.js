c7d49ca0cd4a9656d9693ad05f784481dc76d0df
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-29 09:22:27 -0800

    finished dropdown component

--- lectures/BigReactClass/Section-12/widgets/src/components/App/App.js
+++ lectures/BigReactClass/Section-12/widgets/src/components/App/App.js
@@ -36,10 +36,13 @@ const App = () => {
     ];
 
     const [selected, setSelected] = useState(JSON.stringify(options[0]));
+    const [showDropdown, setShowDropdown] = useState(true);
 
     return (
         <div>
-            <Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {showDropdown ?
+                <Dropdown options={options} selected={selected} onSelectedChange={setSelected} /> : null}
         </div>
     );
 }
--- lectures/BigReactClass/Section-12/widgets/src/components/Dropdown/Dropdown.js
+++ lectures/BigReactClass/Section-12/widgets/src/components/Dropdown/Dropdown.js
@@ -1,11 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    /* When the component is mounted, it adds a click event listener to the body element. When the
+    component is unmounted, it removes the event listener. */
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+
+        document.body.addEventListener("click", onBodyClick, { capture: true });
+
+        return () => {
+            document.body.removeEventListener("click", onBodyClick, {
+                capture: true,
+            });
+        };
+    }, []);
 
     const renderedOptions = options.map((option, index) => {
 
+        /* If the value of the selected option is the same as the value of the option we're rendering,
+        then we return null. Otherwise, we return a div with the label of the option and a click
+        handler that will call the onSelectedChange function with the JSON stringified version of the
+        option. */
         if (option.value === JSON.parse(selected).value) {
             return null
         } else {
@@ -21,7 +45,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     });
 
     return (
-        <div className='ui form'>
+        <div className='ui form' ref={ref}>
             <div className='field'>
                 <label className='label'>Select a Color</label>
                 <div

58ef20d94de4438fac8772aa79a63f380c671555
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-28 19:24:05 -0800

    finished search widget

--- lectures/BigReactClass/Section-12/widgets/src/components/Search/Search.js
+++ lectures/BigReactClass/Section-12/widgets/src/components/Search/Search.js
@@ -3,9 +3,18 @@ import axios from "axios";
 
 const Search = () => {
 
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [debounceTerm, setDebounceTerm] = useState(term)
     const [results, setResults] = useState([]);
 
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebounceTerm(term)
+        }, 1000);
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -15,15 +24,32 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debounceTerm
                 }
             })
             setResults(data.query.search)
         }
-        if (term) {
-            search()
-        }
-    }, [term]);
+        search()
+    }, [debounceTerm])
+
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                        className="ui button">Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div className="ui container">
@@ -36,6 +62,9 @@ const Search = () => {
                         value={term}
                         onChange={(e) => setTerm(e.target.value)} />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );

790ff6896b473194ae746547574228c4234e3c4d
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-29 14:58:58 -0800

    word rendering with underscores for non-guessed letters

--- challenges/hangman/src/components/App/App.js
+++ challenges/hangman/src/components/App/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Puzzle from '../Puzzle/Puzzle';
+import Input from '../Input/Input';
+import GuessedLetters from '../GuessedLetters/GuessedLetters';
 
 const App = () => {
+
+    const words = [
+        "marriageproof",
+        "minionette",
+        "unlichened",
+        "electrocardiographic",
+        "hippophagy",
+        "polyphore",
+        "debellate",
+        "zyga",
+        "antedonin",
+        "hirudinean",
+        "foremastman",
+        "metapolitics",
+        "bianisidine",
+        "gros",
+        "superindifferent",
+        "collar",
+        "maculose",
+        "unphysically",
+        "narrowish",
+        "Bartonia",
+        "inadherent",
+        "arbitrary",
+        "forefeelingly",
+        "palame",
+        "vina",
+        "northwestward",
+        "dog"
+    ]
+
+    // words[Math.floor(Math.random() * words.length)]
+    const [puzzle, setPuzzle] = useState("northwestward");
+    const [guessedLetters, setGuessedLetters] = useState(['n', 't', 'r', 'd']);
+
     return (
         <div>
             <h1>Welcome to Hangman</h1>
-            <Puzzle />
+            <Puzzle puzzle={puzzle} guessedLetters={guessedLetters} />
+            <Input />
+            <GuessedLetters />
         </div>
     );
 }
--- challenges/hangman/src/components/Puzzle/Puzzle.js
+++ challenges/hangman/src/components/Puzzle/Puzzle.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
-const Puzzle = () => {
+const Puzzle = ({ puzzle, guessedLetters }) => {
+
+    const puzzleLetters = puzzle.split('');
+    const renderedPuzzle = puzzleLetters.map((letter, i) => {
+        if (guessedLetters.includes(letter)) {
+            return <span key={i}> {letter} </span>;
+        } else {
+            return <span key={i}> _ </span>;
+        }
+    })
+
     return (
-        <div>Puzzle</div>
+        <div>
+            <h2>{renderedPuzzle}</h2>
+        </div>
     );
 }
 

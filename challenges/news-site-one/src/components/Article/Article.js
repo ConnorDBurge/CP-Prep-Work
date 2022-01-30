67708d3dbcae889703450ae2e3e1b4f692f4d69e
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-30 10:01:30 -0800

    Article component working

--- challenges/news-site-one/src/components/Article/Article.js
+++ challenges/news-site-one/src/components/Article/Article.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class Article extends Component {
-  render() {  
+  render() {
+
+    const { title, created_date, abstract, byline, image } = this.props;
+
     return (
       <div>
+        <h1>{title}</h1>
+        <p>{created_date}</p>
+        {byline ? <h2>{byline}</h2> : null}
+        {image ? <img src={image} /> : null}
+        <p>{abstract}</p>
       </div>
     )
   }

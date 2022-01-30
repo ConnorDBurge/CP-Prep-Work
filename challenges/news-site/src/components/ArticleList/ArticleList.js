68881fabf1d037260d87415580cf18b598e9dbfc
ConnorDBurge <connor.d.burge@gmail.com>
2022-01-30 15:16:53 -0800

    ArticleList component started

---
+++ challenges/news-site/src/components/ArticleList/ArticleList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ArticleTeaser from '../ArticleTeaser/ArticleTeaser.js'
+
+class ArticleList extends Component {
+
+  render() {
+
+    const { articles, handleTitleClick } = this.props;
+
+    const renderedArticleTeasers = articles.map((article, index) => {
+      return (
+        <ArticleTeaser
+          key={index}
+          id={index}
+          title={article.title}
+          created_date={article.created_date}
+          handleTitleClick={(articleID) => { console.log(articleID) }} />
+      )
+    })
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default ArticleList;

37b38209f451bd24ba92b7ad76fea393efc40c18
ConnorDBurge <connor.d.burge@gmail.com>
2022-02-02 18:44:41 -0800

    Article refactered to functional

--- challenges/news-site/src/components/ArticleList/ArticleList.js
+++ challenges/news-site/src/components/ArticleList/ArticleList.js
@@ -1,40 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ArticleTeaser from '../ArticleTeaser/ArticleTeaser.js';
 import { ListGroup, ListGroupItem, InputGroup, Input } from 'reactstrap';
 
-class ArticleList extends Component {
-  render() {
-    const { articles, handleSearch } = this.props;
-    return (
-      <div>
-        <InputGroup>
-          <Input onChange={(e) => handleSearch(e)} type="text" placeholder="Search" />
-        </InputGroup>
-        <ListGroup>
-          {articles.map((article, index) => (
-            <ListGroupItem key={index}>
-              <ArticleTeaser {...article} id={index + 1} />
-            </ListGroupItem>
-          ))}
-        </ListGroup>
-      </div>
-    );
-  }
+const ArticleList = ({ articles, handleSearch }) => {
+  return (
+    <div>
+      <InputGroup>
+        <Input onChange={(e) => handleSearch(e)} type="text" placeholder="Search" />
+      </InputGroup>
+      <ListGroup>
+        {articles.map((article, index) => (
+          <ListGroupItem key={index}>
+            <ArticleTeaser {...article} id={index + 1} />
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    </div>
+  );
 }
 
 export default ArticleList;
-
-
-// Functional solution:
-// function ArticleList({ articles, handleTitleClick }) {
-//   return (
-//     <ListGroup>
-//       {articles.map((article, index) => (
-//         <ListGroupItem>
-//           <ArticleTeaser {...article} id={ index + 1 }
-//              handleTitleClick={handleTitleClick} />
-//         </ListGroupItem>
-//       ))}
-//     </ListGroup>
-//   );
-// }

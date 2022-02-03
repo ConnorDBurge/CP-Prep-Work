9ca874642e1ec6d4689b07e464bc8d653d173ac2
ConnorDBurge <connor.d.burge@gmail.com>
2022-02-02 17:56:03 -0800

    merge

--- challenges/news-site/src/components/ArticleList/ArticleList.js
+++ challenges/news-site/src/components/ArticleList/ArticleList.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import ArticleTeaser from '../ArticleTeaser/ArticleTeaser.js';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, InputGroup, Input } from 'reactstrap';
 
 class ArticleList extends Component {
   render() {
     const { articles } = this.props;
     return (
-      <ListGroup>
-        {articles.map((article, index) => (
-          <ListGroupItem key={index}>
-            <ArticleTeaser {...article} id={index + 1} />
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+      <div>
+        <InputGroup>
+          <Input onChange={(e) => this.handleSearch(e)} type="text" placeholder="Search" />
+        </InputGroup>
+        <ListGroup>
+          {articles.map((article, index) => (
+            <ListGroupItem key={index}>
+              <ArticleTeaser {...article} id={index + 1} />
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
     );
   }
 }

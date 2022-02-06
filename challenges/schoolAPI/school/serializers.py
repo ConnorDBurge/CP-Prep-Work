158ddf27b35f7a5ec05728dd445ca6c7bda0a235
ConnorDBurge <connor.d.burge@gmail.com>
2022-02-06 09:37:35 -0800

    created CRUD functionality for the Course Model

--- challenges/schoolAPI/school/serializers.py
+++ challenges/schoolAPI/school/serializers.py
@@ -21,3 +21,25 @@ class StudentSerializer(object):
                 'first_name': self.data.first_name,
                 'last_name': self.data.last_name
             }
+        
+class CourseSerializer(object):
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def all_courses(self):
+        output = {'courses': []}
+        for course in self.data:
+            course_data = {
+                'course_id': course.course_id,
+                'course_name': course.course_name
+            }
+            output['courses'].append(course_data)
+        return output
+        
+    @property
+    def detail_course(self):
+        return {
+                'course_id': self.data.course_id,
+                'course_name': self.data.course_name
+            }
--- challenges/schoolAPI/school/views.py
+++ challenges/schoolAPI/school/views.py
@@ -3,8 +3,12 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import Student, Course
 from .forms import StudentForm, CourseForm
-from .serializers import StudentSerializer
+from .serializers import StudentSerializer, CourseSerializer
 
+# 
+#
+#
+# Student Routes
 def all_students(request):
     students = Student.objects.all()
     serialized_students = StudentSerializer(students).all_students
@@ -41,3 +45,44 @@ def delete_student(request, student_id):
     student = Student.objects.get(id=student_id)
     student.delete()
     return JsonResponse(data={'message': 'Student deleted'}, status=200)        
+
+#
+#
+#
+# Course Routes
+def all_courses(request):
+    courses = Course.objects.all()
+    serialized_courses = CourseSerializer(courses).all_courses
+    return JsonResponse(data=serialized_courses, status=200)
+
+def detail_course(request, course_id):
+    course = Course.objects.get(id=course_id)
+    serialized_course = CourseSerializer(course).detail_course
+    return JsonResponse(data=serialized_course, status=200)
+
+@csrf_exempt
+def new_course(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        form = CourseForm(data)
+        if form.is_valid():
+            course = form.save(commit=True)
+            serialized_course = CourseSerializer(course).detail_course
+            return JsonResponse(data=serialized_course, status=200)
+        
+@csrf_exempt
+def edit_course(request, course_id):
+    course = Course.objects.get(id=course_id)
+    if request.method == 'POST':
+        data = json.load(request)
+        form = CourseForm(data, instance=course)
+        if form.is_valid():
+            course = form.save(commit=True)
+            serialized_course = CourseSerializer(course).detail_course
+            return JsonResponse(data=serialized_course, status=200)
+        
+@csrf_exempt
+def delete_course(request, course_id):
+    course = Course.objects.get(id=course_id)
+    course.delete()
+    return JsonResponse(data={'message': 'Course deleted'}, status=200)  
